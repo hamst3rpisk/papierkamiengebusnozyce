d815bacb8d9259251563fdca5883fd15b82e5a83
hamst3rpisk <103423521+hamst3rpisk@users.noreply.github.com>
2024-09-29 16:01:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 let humanpick = '';
 
 
-function papier()
-{
+function papier() {
     humanpick = 'p';
     main();
 }
-function kamien()
-{
+
+function kamien() {
     humanpick = 'k';
     main();
 }
-function nozyce()
-{
+
+function nozyce() {
     humanpick = 'n';
     main();
 }
 
+let wincounter = 0;
 
-function main()
-{
+
+
+function main() {
     let pick = Math.floor(Math.random() * 3);
-    let tab = ["papier","kamień","nożyce"];
+
+    let tab = ["papier", "kamień", "nożyce"];
     let charpick = ''
-    let tabchar = ['p','k','n'];
+    let tabchar = ['p', 'k', 'n'];
     charpick = tabchar[pick];
 
     let result = document.querySelector("#result");
-    if (charpick==humanpick)
-    {
+    if (charpick == humanpick) {
         result.innerHTML = "Remis.";
     }
-    if ((charpick == 'p' && humanpick == 'k') || (charpick == 'k' && humanpick == 'n') || (charpick == 'n' && humanpick == 'p'))
-    {
+    if ((charpick == 'p' && humanpick == 'k') || (charpick == 'k' && humanpick == 'n') || (charpick == 'n' && humanpick == 'p')) {
         result.innerHTML = "Wygrał komputer.";
     }
-    if ((humanpick == 'p' && charpick == 'k') || (humanpick == 'k' && charpick == 'n') || (humanpick == 'n' && charpick == 'p'))
-    {
+    if ((humanpick == 'p' && charpick == 'k') || (humanpick == 'k' && charpick == 'n') || (humanpick == 'n' && charpick == 'p')) {
         result.innerHTML = "Wygrał użytkownik.";
+        wincounter++;
     }
-
+    let wins = document.querySelector("#winc");
+    wins.innerHTML = "Wygrałeś już: " + wincounter + " razy";
     let aipick = document.querySelector("#pick");
     aipick.innerHTML = "System wylosował: " + tab[pick];
 }
